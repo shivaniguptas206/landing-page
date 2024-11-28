29c3c6eb1f79f21f6e394b204b5ce354a4f27ff4
Shivani Gupta <shivaniguptas206@gmail.com>
2024-11-28 13:48:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('header nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
